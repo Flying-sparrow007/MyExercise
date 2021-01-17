d00d1a9e6b4d15a4436dc80aba0aef34980602fd
18391026642 <18391026642@163.com>
2021-01-17 23:28:58 +0800

    用户实体类

---
+++ gk_retailers/java/entity/User.java
@@ -0,0 +1,248 @@
+package com.gk.retailers.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 实体类
+ * @author LENOVO
+ *
+ */
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 590534399765914477L;
+	
+	/**用户id*/
+	private Integer id;
+	
+	/**用户姓名*/
+	private String userName;
+	
+	/**用户密码*/
+	private String password;
+	
+	/**用户邮箱*/
+	private String email;
+	
+	/**用户电话*/
+	private String phone;
+	
+	/**用户图片*/
+	private String image;
+	
+	/**用户性别*/
+	private Integer gender;
+	
+	/**创建用户名*/
+	private String createUser;
+	
+	/**创建时间*/
+	private Date createTime;
+	
+	/**修改用户名*/
+	private String modifiedUser;
+	
+	/**修改时间*/
+	private Date modifiedTime;
+
+	public User() {
+		
+	}
+
+	public User(Integer id, String userName, String password, String email, String phone, String image, Integer gender,
+			String createUser, Date createTime, String modifiedUser, Date modifiedTime) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+		this.image = image;
+		this.gender = gender;
+		this.createUser = createUser;
+		this.createTime = createTime;
+		this.modifiedUser = modifiedUser;
+		this.modifiedTime = modifiedTime;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getModifiedUser() {
+		return modifiedUser;
+	}
+
+	public void setModifiedUser(String modifiedUser) {
+		this.modifiedUser = modifiedUser;
+	}
+
+	public Date getModifiedTime() {
+		return modifiedTime;
+	}
+
+	public void setModifiedTime(Date modifiedTime) {
+		this.modifiedTime = modifiedTime;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + ((createUser == null) ? 0 : createUser.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((modifiedTime == null) ? 0 : modifiedTime.hashCode());
+		result = prime * result + ((modifiedUser == null) ? 0 : modifiedUser.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (createTime == null) {
+			if (other.createTime != null)
+				return false;
+		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (createUser == null) {
+			if (other.createUser != null)
+				return false;
+		} else if (!createUser.equals(other.createUser))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (modifiedTime == null) {
+			if (other.modifiedTime != null)
+				return false;
+		} else if (!modifiedTime.equals(other.modifiedTime))
+			return false;
+		if (modifiedUser == null) {
+			if (other.modifiedUser != null)
+				return false;
+		} else if (!modifiedUser.equals(other.modifiedUser))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", email=" + email + ", phone="
+				+ phone + ", image=" + image + ", gender=" + gender + ", createUser=" + createUser + ", createTime="
+				+ createTime + ", modifiedUser=" + modifiedUser + ", modifiedTime=" + modifiedTime + "]";
+	}
+	
+}
