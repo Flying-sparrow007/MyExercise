e186b74ea4508e5b9b86695915b45c22958000f8
18391026642 <18391026642@163.com>
2021-02-08 18:44:22 +0800

    实体类

---
+++ gk_retailers/java/entity/GoodsCategory.java
@@ -0,0 +1,208 @@
+package com.gk.retailers.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class GoodsCategory implements Serializable {
+	
+	private static final long serialVersionUID = -9148631443852849883L;
+	
+	private Integer id;//主键
+	private Integer parentId;//父分类id
+	private String name;//名称
+	private Integer status;//状态   1：正常   2：删除
+	private Integer sortOrder;//排序号
+	private Integer isParent;//是否是父分类   1：是  0：否
+	private Date createdTime;//创建时间
+	private Date modifiedTime;//最后修改时间
+	private String createdUser;//创建人
+	private String modifiedUser;//最后修改人
+	
+	public GoodsCategory() {
+		
+	}
+
+	public GoodsCategory(Integer id, Integer parentId, String name, Integer status, Integer sortOrder, Integer isParent,
+			Date createdTime, Date modifiedTime, String createdUser, String modifiedUser) {
+		super();
+		this.id = id;
+		this.parentId = parentId;
+		this.name = name;
+		this.status = status;
+		this.sortOrder = sortOrder;
+		this.isParent = isParent;
+		this.createdTime = createdTime;
+		this.modifiedTime = modifiedTime;
+		this.createdUser = createdUser;
+		this.modifiedUser = modifiedUser;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Integer parentId) {
+		this.parentId = parentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getSortOrder() {
+		return sortOrder;
+	}
+
+	public void setSortOrder(Integer sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	public Integer getIsParent() {
+		return isParent;
+	}
+
+	public void setIsParent(Integer isParent) {
+		this.isParent = isParent;
+	}
+
+	public Date getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(Date createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public Date getModifiedTime() {
+		return modifiedTime;
+	}
+
+	public void setModifiedTime(Date modifiedTime) {
+		this.modifiedTime = modifiedTime;
+	}
+
+	public String getCreatedUser() {
+		return createdUser;
+	}
+
+	public void setCreatedUser(String createdUser) {
+		this.createdUser = createdUser;
+	}
+
+	public String getModifiedUser() {
+		return modifiedUser;
+	}
+
+	public void setModifiedUser(String modifiedUser) {
+		this.modifiedUser = modifiedUser;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((createdTime == null) ? 0 : createdTime.hashCode());
+		result = prime * result + ((createdUser == null) ? 0 : createdUser.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((isParent == null) ? 0 : isParent.hashCode());
+		result = prime * result + ((modifiedTime == null) ? 0 : modifiedTime.hashCode());
+		result = prime * result + ((modifiedUser == null) ? 0 : modifiedUser.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((parentId == null) ? 0 : parentId.hashCode());
+		result = prime * result + ((sortOrder == null) ? 0 : sortOrder.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GoodsCategory other = (GoodsCategory) obj;
+		if (createdTime == null) {
+			if (other.createdTime != null)
+				return false;
+		} else if (!createdTime.equals(other.createdTime))
+			return false;
+		if (createdUser == null) {
+			if (other.createdUser != null)
+				return false;
+		} else if (!createdUser.equals(other.createdUser))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (isParent == null) {
+			if (other.isParent != null)
+				return false;
+		} else if (!isParent.equals(other.isParent))
+			return false;
+		if (modifiedTime == null) {
+			if (other.modifiedTime != null)
+				return false;
+		} else if (!modifiedTime.equals(other.modifiedTime))
+			return false;
+		if (modifiedUser == null) {
+			if (other.modifiedUser != null)
+				return false;
+		} else if (!modifiedUser.equals(other.modifiedUser))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (parentId == null) {
+			if (other.parentId != null)
+				return false;
+		} else if (!parentId.equals(other.parentId))
+			return false;
+		if (sortOrder == null) {
+			if (other.sortOrder != null)
+				return false;
+		} else if (!sortOrder.equals(other.sortOrder))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "GoodsCategory [id=" + id + ", parentId=" + parentId + ", name=" + name + ", status=" + status
+				+ ", sortOrder=" + sortOrder + ", isParent=" + isParent + ", createdTime=" + createdTime
+				+ ", modifiedTime=" + modifiedTime + ", createdUser=" + createdUser + ", modifiedUser=" + modifiedUser
+				+ "]";
+	}
+	
+	
+}
