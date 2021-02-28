a2394078cf6d68885189895fbd4966a1f18cceb5
18391026642 <18391026642@163.com>
2021-02-28 12:18:38 +0800

    业务层

--- gk_retailers/java/service/goodsCategory/GoodsCategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.gk.retailers.service.goodsCategory;
-
-import java.util.Map;
-
-public interface GoodsCategoryService {
-	/**根据分类的父Id查找相关的商品*/
-	Map<String, Object> findGoodsCategoryByParentIdService(Integer pId, Integer start, Integer pageSize);
-}
